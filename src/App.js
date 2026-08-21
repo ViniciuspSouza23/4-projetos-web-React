ac136f790789aea9944ab9bd750029a2705e0d78
VinicinhoPereira23 <vinicius.souza.1bmangal@gmail.com>
2026-08-20 21:49:37 -0300

    feat: adiciona projeto React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+// src/Header.js
+function Header() {
+  return (
+    <header className="App-header">
+      <h1>📚 Biblioteca Virtual</h1>
+      <p>Empreste, devolva e organize suas leituras</p>
+    </header>
+  );
+}
+
+export default Header;
